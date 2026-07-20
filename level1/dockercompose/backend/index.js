097d6ce263050d03f1d9db096984b5b96699813d
SyedYasirAliZafar <yasirali1234zafar@gmail.com>
2026-07-21 03:40:35 +0500

    Api Gateway Done (Microservices)

--- level1/dockercompose/backend/index.js
+++ level1/dockercompose/backend/index.js
@@ -1,16 +1,14 @@
-import express from "express"
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
+const port = process.env.PORT || 5000;
+const app = express();
 
-const port = process.env.PORT || 5000
-const app = express()
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Hello from Auth Service " });
+});
 
-app.get("/", (req, res)=>{
-    return res.status(200).json({message:'Hello from phase 2'})
-})
-
-app.listen(port,()=>{
-    console.log("Server Started");
-    
-})
+app.listen(port, () => {
+  console.log("Server Started");
+});
